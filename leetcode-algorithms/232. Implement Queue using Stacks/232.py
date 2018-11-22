b380455a23543eaf34a904da2d0d0ffe6da4bccc
liubaopeng <liubaopeng@jd.com>
2018-11-22 22:12:46 +0800

    232

--- leetcode-algorithms/232. Implement Queue using Stacks/232.py
+++ leetcode-algorithms/232. Implement Queue using Stacks/232.py
@@ -1,10 +1,13 @@
 class MyQueue:
-
+    """
+    重点在于_move方法,把先进入栈中元素,依次移入另一个栈中,从而达到先进先出的效果
+    """
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.stack = []
+        self.in_stack = []
+        self.out_stack = []
 
     def push(self, x):
         """
@@ -12,28 +15,35 @@ class MyQueue:
         :type x: int
         :rtype: void
         """
-        self.stack.append(x)
+        self.in_stack.append(x)
 
     def pop(self):
         """
         Removes the element from in front of queue and returns that element.
         :rtype: int
         """
-        return self.stack.pop()
+        self._move()
+        return self.out_stack.pop()
 
     def peek(self):
         """
         Get the front element.
         :rtype: int
         """
-        return self.stack[-1]
+        self._move()
+        return self.out_stack[-1]
 
     def empty(self):
         """
         Returns whether the queue is empty.
         :rtype: bool
         """
-        return len(self.stack) > 0
+        return (not self.in_stack) and (not self.out_stack)
+
+    def _move(self):
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
 
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
